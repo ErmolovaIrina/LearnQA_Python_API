17293cbb8b4cbbda253f7594bcfe324e97430ec7
ErmolovaIrina <i.ermolova@raceit.ru>
2023-02-13 20:33:42 +0300

    тест получения куки

--- test_get_cookie.py
+++ test_get_cookie.py
@@ -3,11 +3,11 @@ import requests
 class TestResponseCookie:
   def test_get_cookie(self):
 
-      response = requests.get("https://playground.learnqa.ru/api/homework_cookies")
+      response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
       cookie_value = dict(response.cookies)
       print(cookie_value)
 
       assert "HomeWork" in response.cookies, f"There is no cookie with 'HomeWork' key in response"
 
       expected_cookie_value = "hw_value"
-      assert expected_cookie_value in response.cookies.values(), f"There is not cookie with value {expected_cookie_value} in response"
+      assert expected_cookie_value in response.cookies.values(), f"There is no cookie with value {expected_cookie_value} in response"

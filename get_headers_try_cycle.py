a116b46cb8102d2f3d1cbb9670d00028b1b098f4
ErmolovaIrina <i.ermolova@raceit.ru>
2023-02-16 21:55:54 +0300

    тест получения хедеров,эксперименты

---
+++ get_headers_try_cycle.py
@@ -0,0 +1,18 @@
+import requests
+
+class TestHeader:
+  def test_get_headers_try_cycle(self):
+
+      response = requests.get("https://playground.learnqa.ru/api/homework_header")
+      header_value = response.headers
+      print(header_value)
+
+      expected_result = {'Content-Type':'application/json', 'Connection':'keep-alive', 'Server':'Apache', 'x-secret-homework-header':'Some secret value'}
+
+      for key in expected_result.keys():
+        print(key)
+        assert key in response.headers, f"There is no header with key '{key}' in response"
+
+      for value in expected_result.values():
+        print(value)
+        assert value in response.headers, f"There is no header with key '{value}' in response"

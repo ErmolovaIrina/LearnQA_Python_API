586f65dd0c7c5f62f38e44d99ee3021ef5baacf2
ErmolovaIrina <i.ermolova@raceit.ru>
2023-02-21 23:18:32 +0300

    Задание 16

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -16,7 +16,7 @@ class TestUserAuth(BaseCase):
 
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
         self.auth_sid = self.get_cookie(response1, "auth_sid")
-        self.token = self.get_header(response1, "token")
+        self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,6 +1,23 @@
 import requests
+import random
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 class TestUserGet(BaseCase):
     def test_user_details_with_other_auth(self):
+        data = {
+          'email': 'vinkotov@example.com',
+          'password' : "1234"
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+
+        response2 = requests.get("https://playground.learnqa.ru/api/user/5",
+            headers={"x-csrf-token": self.token} ,
+            cookies={"auth_sid": self.auth_sid})
+
+        Assertions.assert_json_has_keys()

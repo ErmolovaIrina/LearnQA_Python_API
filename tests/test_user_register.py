35feff0039fbed94950e1cd7d7e2801679231058
ErmolovaIrina <i.ermolova@raceit.ru>
2023-02-25 22:30:50 +0300

    Аллюр теги

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,5 +1,5 @@
 import string
-
+import allure
 import pytest
 import requests
 import random
@@ -8,8 +8,9 @@ from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from datetime import datetime
 
-
+@allure.epic("Регистрация юзера")
 class TestUserRegister(BaseCase):
+    @allure.description("Подготовительный метод")
     def setup_method(self):
         basePart = "base"
         domain = "learnqa.com"
@@ -24,6 +25,7 @@ class TestUserRegister(BaseCase):
                 ({"password": "123", "username": "Ivan123", "firstName": "Ivan", "email": "self.correct_email"}, "The following required params are missed: lastName"),
                 ({"password": "123", "username": "Ivan123", "firstName": "Ivan", "lastName": "Ivanov"}, "The following required params are missed: email")]
 
+    @allure.description("Создание пользователя с невалидным имейлом")
     def test_create_user_with_wrong_email(self):
         data=self.prerare_registration_with_wrong_email()
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
@@ -32,6 +34,7 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"Invalid email format", Assertions.assert_json_has_key(response, "id")
 
 
+    @allure.description("Параметризированный тест с попыткой создания юзера с недостающими параметрами")
     @pytest.mark.parametrize("data, expected_message", testData)
     def test_create_user_with_wrong_request(self, data, expected_message):
 
@@ -40,6 +43,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         Assertions.assert_invalid_request_message(response, expected_message)
 
+    @allure.description("Создание юзера с очень коротким невалидным именем")
     def test_create_user_with_short_name(self):
         data={
             "password":"123",
@@ -55,6 +59,7 @@ class TestUserRegister(BaseCase):
         error_message = "The value of 'username' field is too short"
         Assertions.assert_invalid_request_message(response, error_message)
 
+    @allure.description("Создание юзера с очень длинными именем")
     def test_create_user_with_long_name(self):
 
         letters = string.ascii_letters

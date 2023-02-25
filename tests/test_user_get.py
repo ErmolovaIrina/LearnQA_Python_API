1bacb44d4b8dba240fb04a14bb105efe97b86ca7
ErmolovaIrina <i.ermolova@raceit.ru>
2023-02-25 22:26:14 +0300

    Аллюр теги

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,22 +1,25 @@
 import requests
-import random
+import allure
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+@allure.feature("Получение информации о юзере")
 class TestUserGet(BaseCase):
+    @allure.description("Получение информации о юзере с авторизацией под другим")
     def test_user_details_with_other_auth(self):
         data = {
           'email': 'vinkotov@example.com',
           'password' : "1234"
         }
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
 
-        response2 = requests.get("https://playground.learnqa.ru/api/user/5",
+        response2 = MyRequests.get("/user/5",
             headers={"x-csrf-token": self.token} ,
             cookies={"auth_sid": self.auth_sid})
         print(response2.text)
